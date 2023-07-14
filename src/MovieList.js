d9abb2c2847a89a606d26b37c3c13296707b481f
Whiskas101 <getintouchwithvic@gmail.com>
2023-07-14 20:46:02 +0530

    Fix
    did some changes, error doesnt show up anymore. Have to figure out how to show that data on screen?

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,25 +1,19 @@
+import { json } from "react-router-dom";
 import useFetch from "./UseFetch";
 
-const MovieList =() => {
-        const {data:movies,isPending} = useFetch("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc");
-        console.log(movies)
+const MovieList = () => {
+        let [movies, isPending] = useFetch("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc");
+        //console.log(movies)
         
-        // return (
-            
-        //     <div className="movie-list" key='id'>
-        //         {
-        //         movies.map(function(movie){
-        //             if(!movies){
-        //             return (
-        //                 (
-        //                 <div className="movie">
-        //                     {console.log(movie.title)}
-        //                 </div>
-        //                 )
-        //             )}
-        //         })}
-        //     </div>
-        // );
+
+        return <>
+        {
+                movies.map((movie)=>{
+                        console.log(movie.id);
+                        <div key={movie}>aha</div>
+                })
+        }
+        </>
     
 }
  
