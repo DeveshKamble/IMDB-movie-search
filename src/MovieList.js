ee893f066e404f985fc62bfcb38f0cfb99b3f8ef
DeveshKamble <d.kamble@iitg.ac.in>
2023-07-15 12:16:45 +0530

    Data is being displayed now. Need to create a function to gt genre from genre id

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,20 +1,63 @@
-import { json } from "react-router-dom";
-import useFetch from "./UseFetch";
+import { useState,useEffect } from 'react';
+
 
 const MovieList = () => {
-        let [movies, isPending] = useFetch("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc");
-        //console.log(movies)
+        
+        const [movies, setMovies] = useState([]);
+        const [genreList, setGenreList] = useState([])
+        // const [error, setError] = useState(null);
+        useEffect(()=>{
+                fetch('https://api.themoviedb.org/3/genre/movie/list', {method: 'GET',headers: {accept: 'application/json',Authorization: `Bearer ${process.env.REACT_ACCESS_TOKEN}` }})
+                        .then(res => res.json())
+                        .then(json => { 
+                                console.log(json.genres)
+                                return setGenreList(json.genres)
+                        })
+                        .catch(err => console.error('error:' + err));
+        },[])
+
+        // function getGenre(given_id){
+        //         const element = genreList.filter((genre)=>genre.id === given_id);
+        //         return element.name;
+        // }
         
 
-        return <>
-        {
-                movies.map((movie)=>{
-                        console.log(movie.id);
-                        <div key={movie}>aha</div>
+        
+
+        useEffect(() => {
+                fetch("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc", {
+                method : "GET",
+                headers : {
+                        'Authorization': `Bearer ${process.env.REACT_ACCESS_TOKEN}`,
+                        'accept': 'application/json'
+                }
                 })
-        }
-        </>
-    
+                .then(response => {return response.json()})
+                
+                .then((result)=>{
+                        result.results.length = result.results.length - (result.results.length%3)
+                        setMovies(result.results)
+                //console.log(result);
+                })
+                
+        })
+
+        
+        
+        return (
+                <div className="movie-list">
+                    {movies.map((movie)=> (
+                    <div className="movie" key={movie.id}>
+                        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="poster" height={400} width = {300}/>
+                        <p> { movie.genre_ids[0] } </p>
+                        <p> { movie.title } </p>
+                        
+
+                    </div>
+                ))}
+                </div>
+            );
+
 }
  
 export default MovieList;
